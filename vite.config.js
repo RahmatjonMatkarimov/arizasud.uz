52ff547f79d80cbd5a099839ceb458ec90a531f0
Rahmatjon <rahmatjon@192.168.1.9>
2025-04-06 21:10:55 +0500

    1

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), // Vue komponentlariga to'g'ri yo'naltirish
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "pdfjs-dist": "pdfjs-dist/legacy/build/pdf", // Legacy build orqali to'g'ri yo'naltirish
     },
   },
   server: {
-    host: '0.0.0.0',  // Hammasi uchun ochiq bo'lishi uchun
-    port: 5001,  // Portni o'zgartirishingiz mumkin
-    strictPort: true,  // Agar ko'rsatilgan port band bo'lsa, serverni boshlashni rad etadi
-    cors: {
-      origin: '*' // CORS siyosati (agar kerak bo'lsa)
-    },
+    host: "0.0.0.0",
+    port: 5001,
+    strictPort: true,
+    cors: true,
     watch: {
-      usePolling: true, // File system monitoringni yaxshilash uchun pollingni yoqish
+      usePolling: true, // Windows yoki konteynerlarda ishlatiladi
     },
-  },
-  build: {
-    // Build jarayonini optimallashtirish
-    outDir: 'dist',  // Build natijalarining chiqariladigan manzili
-    sourcemap: true,  // Source map qo'shish (tezda xatolarni tuzatish uchun foydali)
   },
 });
