6e1542725128dc51d23cf9d8b3c28956321139b8
? <rahmatjon@192.168.0.181>
2025-06-23 10:39:03 +0500

    1

--- src/components/qrcode/WerehouseOpenRoom.vue
+++ src/components/qrcode/WerehouseOpenRoom.vue
@@ -289,7 +289,7 @@
 <script setup>
 import translateText from '@/auth/Translate'
 import { URL } from '@/auth/url'
-import { ref, onMounted, inject } from 'vue'
+import { ref, onMounted, inject, onUnmounted } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
