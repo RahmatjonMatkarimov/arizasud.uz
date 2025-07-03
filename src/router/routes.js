e84d21c1acb5c81534bb3709e22fe9b4761a299b
Rahmatjon Matkarimov <rahmatjon@192.168.0.181>
2025-07-03 18:52:06 +0500

    1

--- src/router/routes.js
+++ src/router/routes.js
@@ -45,7 +45,7 @@ const routes = [
       { path: '/link', component: () => import('@/components/dashboard/links.vue') },
       { path: '/profile', component: () => import('@/views/Profile.vue') },
     ],
-    meta: { requiresAuth: true, allowedRoles: ['accauntant','chiefAccauntant', 'bigAdmin'] },
+    // meta: { requiresAuth: true, allowedRoles: ['accauntant','chiefAccauntant', 'bigAdmin'] },
   },
   {
     path: '/admin',
@@ -129,7 +129,7 @@ const routes = [
         props: true
       },
     ],
-    meta: { requiresAuth: true, allowedRoles: ['admin', 'manager','warehouseman','chiefAccauntant', 'yuristAssistant', 'accauntant', 'deliverer', 'yurist', 'bigAdmin'] },
+    // meta: { requiresAuth: true, allowedRoles: ['admin', 'manager','warehouseman','chiefAccauntant', 'yuristAssistant', 'accauntant', 'deliverer', 'yurist', 'bigAdmin'] },
   },
 ];
 export default routes
